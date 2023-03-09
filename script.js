5934bb4b45053d15434c3f2942057a6ca81f24f3
WalkingInTheFog <nkpl@list.ru>
2023-03-09 13:02:07 +0300

    переключение слайдера сделал

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ let fillValueField = function (form, name, email) {
 
 };
 
-
 let modalHandler = function (openBtn, modalBlock, closeBtn, toogleClass, animateClass) {
 
     openBtn.addEventListener('click', function (evt) {
@@ -71,6 +70,11 @@ let openClass = 'pop-up--show';
 let AnimateClass = 'animate__fadeInDownBig';
 let form = modalWindow.querySelector('.pop-up__form');
 
+let slider = document.querySelector('.slider-wrapper');
+let controlBtnsSlider = slider.querySelectorAll('.main-slider__controls-btn');
+let slides = slider.querySelectorAll('.slider-list__item');
+let slideShowClass = 'slider-list__item--current';
+let btnCurrentClass = 'main-slider__controls-btn--current';
 /*------------------------------------Code-------------------------------------------------------- */
 
 /*----если переменные нашлись, то на кнопки функцией вешаются события клика, функция добавляет класс открытия и анимации---- */
@@ -92,5 +96,36 @@ form.addEventListener('submit', function (evt) {
 
 });
 
+/*------------------------------slider---------------------------------------------*/
 
+if (controlBtnsSlider && slides) {
+
+    for (let btn = 0; btn < controlBtnsSlider.length; btn++) {
+
+        controlBtnsSlider[btn].addEventListener('click', function (evt) {
+
+            if (!evt.target.classList.contains(btnCurrentClass)) {
+
+                for (let slide = 0; slide < slides.length; slide++) {
+
+                    slides[slide].classList.remove(slideShowClass);
+
+                }
+
+                slides[btn].classList.add(slideShowClass);
+
+                for (let i = 0; i < controlBtnsSlider.length; i++) {
+
+                    controlBtnsSlider[i].classList.remove(btnCurrentClass);
+
+                }
+
+                controlBtnsSlider[btn].classList.add(btnCurrentClass);
+            }
+
+        });
+
+    };
+
+}
 
